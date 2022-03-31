c3e2e8a7e9a6696e15d86c39598bdcc1e8d0a00e
muradav <muradav@code.edu.az>
2022-03-31 16:54:25 +0400

    feat:currency taski hazirdir

---
+++ CurrencyEnum/CurrencyEnum/Helper/CurrencyHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyEnum.Helper
+{
+    enum Currency {USD,EUR,TRY}
+    class CurrencyHelper
+    {
+        public void Exchange(Enum obj, double azn)
+        {
+            bool testenum = false;
+            if(obj is Currency)
+            {
+                testenum =true;
+                switch (obj)
+                {
+                    case Currency.USD:
+
+                        Console.WriteLine($"{azn} AZN = {azn * 0.59} USD");
+                        break;
+
+                    case Currency.EUR:
+
+                        Console.WriteLine($"{azn} AZN = {azn * 0.53} EUR");
+                        break;
+
+                    case Currency.TRY:
+
+                        Console.WriteLine($"{azn} AZN = {azn * 8.62} TYR");
+                        break;
+
+                }               
+
+                
+            }
+            else
+            {
+                Console.WriteLine("Currency is not exist");
+            }
+            
+            
+
+
+
+
+        }
+    }
+}
---
+++ CurrencyEnum/CurrencyEnum/Program.cs
@@ -0,0 +1,20 @@
+﻿using CurrencyEnum.Helper;
+using System;
+
+namespace CurrencyEnum
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            CurrencyHelper nc = new CurrencyHelper();
+            nc.Exchange(Currency.EUR, 170);
+           
+        }
+
+
+            
+    }
+}
